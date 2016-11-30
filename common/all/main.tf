9252f2bd7ed8eb5ad2fda1415822206d52a40141
ntk1000 <jun22011982@gmail.com>
2016-12-01 00:41:15 +0900

    fix common resource

--- common/all/main.tf
+++ common/all/main.tf
@@ -12,22 +12,22 @@ module "billing-alert" {
     email_sns_subscription_arn_us = "${var.email_sns_subscription_arn_us}"
 }
 
-module "vpc" {
-    source = "../../modules/vpc"
-    cidr = "${var.cidr}"
-    project = "${var.project}"
-    env = "${var.env}"
-}
-
 module "common" {
     source = "../../"
     team = "${var.team}"
     email_sns_subscription_arn = "${var.email_sns_subscription_arn}"
-    gip_1 = "${var.gip_1}"
-    gip_2 = "${var.gip_2}"
-    gip_3 = "${var.gip_3}"
     email_sns_subscription_arn_us = "${var.email_sns_subscription_arn_us}"
     project = "${var.project}"
     env = "${var.env}"
+    cidr-public-aza = "${var.cidr-public-aza}"
+    cidr-public-azc = "${var.cidr-public-azc}"
+    cidr-protected-aza = "${var.cidr-protected-aza}"
+    cidr-protected-azc = "${var.cidr-protected-azc}"
+    cidr-private-aza = "${var.cidr-private-aza}"
+    cidr-private-azc = "${var.cidr-private-azc}"
+    gip_1 = "${var.gip_1}"
+    gip_2 = "${var.gip_2}"
+    gip_3 = "${var.gip_3}"
+    vpc-id = "${var.vpc_id_common}"
 }
 
--- common/all/variables.tf
+++ common/all/variables.tf
@@ -3,11 +3,27 @@ variable "project" {}
 variable "env" {}
 
 variable "email_sns_subscription_arn" {}
+variable "email_sns_subscription_arn_us" {}
 variable "gip_1" {}
 variable "gip_2" {}
 variable "gip_3" {}
-variable "email_sns_subscription_arn_us" {}
+variable "vpc_id_common" {}
 
-variable "cidr" {
-    default = "10.0.0.0/16"
+variable "cidr-public-aza" {
+    default = "10.0.0.0/23"
+}
+variable "cidr-public-azc" {
+    default = "10.0.2.0/23"
+}
+variable "cidr-protected-aza" {
+    default = "10.0.8.0/23"
+}
+variable "cidr-protected-azc" {
+    default = "10.0.10.0/23"
+}
+variable "cidr-private-aza" {
+    default = "10.0.16.0/23"
+}
+variable "cidr-private-azc" {
+    default = "10.0.18.0/23"
 }
