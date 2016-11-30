e9b6d4bfc6f4ccf8bf2ebcc4f56b8427630cd242
ntk1000 <jun22011982@gmail.com>
2016-11-30 15:58:15 +0900

    add prod

--- devops/prod/main.tf
+++ devops/prod/main.tf
@@ -1,13 +1,10 @@
-module "vpc" {
-    source = "../../modules/vpc"
-    cidr = "${var.cidr}"
-    project = "${var.project}"
-    env = "${var.env}"
-}
-
 module "prod" {
     source = "../../"
     team = "${var.team}"
     project = "${var.project}"
     env = "${var.env}"
+    cidr = "${var.cidr}"
+    gip_1 = "${var.gip_1}"
+    gip_2 = "${var.gip_2}"
+    gip_3 = "${var.gip_3}"
 }
--- devops/prod/variables.tf
+++ devops/prod/variables.tf
@@ -2,6 +2,29 @@ variable "team" {}
 variable "project" {}
 variable "env" {}
 
+variable "gip_1" {}
+variable "gip_2" {}
+variable "gip_3" {}
+
 variable "cidr" {
     default = "10.1.0.0/16"
+}
+
+variable "cidr-public-aza" {
+    default = "10.1.0.0/23"
+}
+variable "cidr-public-azc" {
+    default = "10.1.2.0/23"
+}
+variable "cidr-protected-aza" {
+    default = "10.1.8.0/23"
+}
+variable "cidr-protected-azc" {
+    default = "10.1.10.0/23"
+}
+variable "cidr-private-aza" {
+    default = "10.1.16.0/23"
+}
+variable "cidr-private-azc" {
+    default = "10.1.18.0/23"
 }
